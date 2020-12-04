cef0bd8dffb7a6e82dc0f92f086be3322823d250
Samuil Ganev <57064926+samuil-ganev@users.noreply.github.com>
2020-12-04 14:02:20 +0200

    Update City.java

--- 17/City.java
+++ 17/City.java
@@ -2,13 +2,34 @@ class City {
 
   private String name;
   private int population;
+  private Citizen[] citizens;
   private String country;
 
-  City(String name, int population, String country) {
+  City(String name, String country) {
+
+    this.name = name;
+    this.country = country;
+
+  }
+
+  City(String name, int population, Citizen[] citizens, String country) {
 
     this.name = name;
     this.population = population;
+    this.citizens = citizens;
     this.country = country;
+
+  }
+
+  public Citizen[] getCitizens() {
+
+    return this.citizens;
+
+  }
+
+  public void setCitizens(Citizen[] citizens) {
+
+    this.citizens = citizens;
 
   }
 
@@ -45,6 +66,18 @@ class City {
   public void setCountry(String name) {
 
     this.country = name;
+
+  }
+
+  public void increacePopulation(int num) {
+
+    this.population += num;
+
+  }
+
+  public void decreacePopulation(int num) {
+
+    this.population -= num;
 
   }
 

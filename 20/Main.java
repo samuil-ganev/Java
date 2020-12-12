5fb96b666bcae70810518a1718848ac2a6b58905
Samuil Ganev <57064926+samuil-ganev@users.noreply.github.com>
2020-12-13 00:48:18 +0200

    Update Main.java

--- 20/Main.java
+++ 20/Main.java
@@ -3,7 +3,7 @@ public class Main {
     public static void main(String[] args) {
        
         // Declare and create Product array of size 5
-        Product[] pa = new Product[6];
+        Product[] pa = new Product[7];
        
         // Create object instances
         pa[0] = new TV(1000, "Samsung", 30);
@@ -12,6 +12,7 @@ public class Main {
         pa[3] = new Book(34, "Sun press", 1992);
         pa[4] = new Book(15, "Korea press", 1986);
         pa[5] = new ChildrenBook(15, "PW press", 1987, 8);
+        pa[6] = new ComicBook(14, "PW press", 1924, "Batman");
 
         // Compute total regular price and total
         // sale price.

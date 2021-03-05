0c691893e506d34eb7577c820ea45827d1ada5b9
Samuil Ganev <57064926+samuil-ganev@users.noreply.github.com>
2021-03-05 07:37:50 +0200

    Create SierpinskiTestProgram.java

---
+++ Algorithms/Recursion/6/SierpinskiTestProgram.java
@@ -0,0 +1,18 @@
+package schl;
+
+import javax.swing.JFrame;
+
+public class SierpinskiTestProgram {
+
+	public static void main(String[] args) {
+  
+		SierpinskiTriangleFrame frame = new SierpinskiTriangleFrame();
+		frame.setSize(400, 400);
+		frame.setTitle("TestFigurePanel");
+		frame.setLocationRelativeTo(null);
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		frame.setVisible(true);
+	
+  }
+
+}
